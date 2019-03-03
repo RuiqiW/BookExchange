99b13b7864bda1ecbe9d63063c9ec019a19993e3
Eric K <eric.koehli@mail.utoronto.ca>
2019-03-03 09:49:34 -0500

    Moved classes Post and Transaction into their own js module.

---
+++ classes/Post.js
@@ -0,0 +1,131 @@
+import { users } from "./User";
+
+
+/**
+ * This module contains a Post class for user's when they post an ad.
+ */
+
+let postId = 0;
+export class Post {
+  constructor(title, seller){
+    this._postId = postId;
+    this._image = "";
+    this._category = "Textbook";
+    this._condition = "New";
+    this._description = "";
+    this._price = "$10";
+    postId ++;
+    this._isSold = false;
+    //Should bind with the corresponding transaction if there is one
+    this._transaction = null;
+    this._title = title;
+    this._seller = seller;
+    seller.sell.push(this);
+  }
+
+  get title() {
+    return this._title;
+  }
+
+
+  get seller() {
+    return this._seller;
+  }
+
+
+  get postId() {
+    return this._postId;
+  }
+
+
+  get image() {
+    return this._image;
+  }
+
+  set image(value) {
+    this._image = value;
+  }
+
+  get category() {
+    return this._category;
+  }
+
+  set category(value) {
+    this._category = value;
+  }
+
+  get condition() {
+    return this._condition;
+  }
+
+  set condition(value) {
+    this._condition = value;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  set description(value) {
+    this._description = value;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(value) {
+    this._price = value;
+  }
+
+  get isSold() {
+    return this._isSold;
+  }
+
+  set isSold(value) {
+    this._isSold = value;
+  }
+
+  get transaction() {
+    return this._transaction;
+  }
+
+  set transaction(value) {
+    this._transaction = value;
+  }
+}
+
+
+// Constant Posts for phase 1
+const posts =[];
+const post1 = new Post("Calculus", users[0]);
+post1.image = "../images/admin/textbook1.jpg";
+posts.push(post1);
+
+const post2 = new Post("Algorithms", users[2]);
+post2.image = "../images/admin/textbook2.jpg";
+posts.push(post2);
+
+const post3 = new Post("Chez Nous", users[2]);
+post3.image = "../images/admin/textbook3.jpg";
+posts.push(post3);
+
+const post4 = new Post("Microeconomics", users[0]);
+post4.image = "../images/admin/textbook4.jpg";
+posts.push(post4);
+
+const post5 = new Post("Statistics", users[0]);
+post5.image = "../images/admin/textbook5.jpg";
+posts.push(post5);
+
+const post6 = new Post("Web Programming", users[0]);
+posts.push(post6);
+
+const post7 = new Post("Linear Algebra", users[2]);
+posts.push(post7);
+
+
+
+
+
+export{ posts };
