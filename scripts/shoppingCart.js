47614f6e42b8f72b2d9b383f99fa766d9b3da181
will <will.qie@mail.utoronto.ca>
2019-03-05 21:52:44 -0500

    add stuff to shopping cart page

--- scripts/shoppingCart.js
+++ scripts/shoppingCart.js
@@ -150,3 +150,51 @@ function searching(e) {
     document.location = "../pages/items.html";
 }
 
+/*********************** Chat Box ************************/
+
+const chat = document.querySelector('#chat');
+const sendButton = document.querySelector("#sendButton");
+sendButton.addEventListener('click', sendMessage);
+
+function sendMessage(e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains("submit")) {
+        const message = document.querySelector("#messageBox").value;
+        if (message.length > 0 && message.length < 200) {
+            addMessage(message);
+        }
+    }
+    chat.scrollTop = chat.scrollHeight;
+}
+
+
+// helper function for sendMessage, add message to chat window
+function addMessage(msg) {
+    const newMessage = document.createElement('p');
+    newMessage.className = "chatOutText";
+    newMessage.innerText = msg;
+    const bubble = document.createElement('div');
+    bubble.className = "chatOutBubble";
+    bubble.appendChild(newMessage);
+    const messageContainer = document.createElement('div');
+    messageContainer.appendChild(bubble);
+    chat.appendChild(messageContainer);
+}
+// Show / Hide chatbox
+const chatShow = document.querySelector("#chatShow");
+const chatHide = document.querySelector("#chatHide");
+chatHide.addEventListener('click', hideChatRoom);
+chatShow.addEventListener('click', showChatRoom);
+
+function showChatRoom(e) {
+    e.preventDefault();
+    const chatRoom = document.querySelector('#chatRoom');
+    chatRoom.style.display = "block";
+}
+
+function hideChatRoom(e) {
+    e.preventDefault();
+    const chatRoom = document.querySelector('#chatRoom');
+    chatRoom.style.display = "none";
+}
