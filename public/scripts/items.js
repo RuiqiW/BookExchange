459ea81c6c933a7346ad6fa33b5d2d9dd3705bb2
Lun Yu Li <lunyu.li@mail.utoronto.ca>
2019-03-24 23:09:04 -0400

    added search option box

--- public/scripts/items.js
+++ public/scripts/items.js
@@ -357,3 +357,75 @@ function searching(e) {
     //Server call to request search result, here just jump to the item.html;
     document.location = "../pages/items.html";
 }
+
+/*********************** Drop down select ************************/
+
+// the select drop down box with 3 search options
+const x = document.getElementsByClassName("custom-select");
+for (let i = 0; i < x.length; i++) {
+    const selElmnt = x[i].getElementsByTagName("select")[0];
+    // for each element, create a div DIV that will act as the selected item
+    const a = document.createElement("div");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    // for each element, create a new div that will contain the option list
+    const b = document.createElement("div");
+    b.setAttribute("class", "select-items select-hide");
+    for (let j = 1; j < selElmnt.length; j++) {
+        // for each option in the original select element, create a new div that will act as an option item
+        const c = document.createElement("div");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function(e) {
+            // when an item is clicked, update the original select box and the selected item
+            const s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            const h = this.parentNode.previousSibling;
+            for (let l = 0; l < s.length; l++) {
+                if (s.options[l].innerHTML === this.innerHTML) {
+                    s.selectedIndex = l;
+                    h.innerHTML = this.innerHTML;
+                    const y = this.parentNode.getElementsByClassName("same-as-selected");
+                    for (let k = 0; k < y.length; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+            }
+            h.click();
+        });
+        b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function(e) {
+        // when the select box is clicked, close any other select boxes and open/close the current select box
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+    });
+}
+
+// closes the select box
+function closeAllSelect(elmnt) {
+    /*a function that will close all select boxes in the document,
+    except the current select box:*/
+    const arrNo = [];
+    const x = document.getElementsByClassName("select-items");
+    const y = document.getElementsByClassName("select-selected");
+    for (let i = 0; i < y.length; i++) {
+        if (elmnt === y[i]) {
+            arrNo.push(i)
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
+    }
+    for (let j = 0; j < x.length; j++) {
+        if (arrNo.indexOf(j)) {
+            x[j].classList.add("select-hide");
+        }
+    }
+}
+
+// Close all select boxes if the user clickes outside of the box
+document.addEventListener("click", closeAllSelect);
