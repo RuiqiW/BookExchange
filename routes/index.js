176c2e291bdec8fbeca9f30ac37e627daa11e3cc
Eric K <eric.koehli@mail.utoronto.ca>
2019-03-30 15:11:37 -0400

    got routes to work

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 
 /** Get all the other routers */
@@ -15,15 +16,18 @@ router.use('/users', usersRouter);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '../public/index.html');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+  // res.sendFile(path.join(__dirname, '../public/styles/index.css'));
+  // res.sendFile(path.join(__dirname, '../public/scripts/index.js'));
 });
-
-
 
 
 router.get('/404', (req, res, next) => {
-  res.sendFile(__dirname + '../public/pages.404.html');
+  res.sendFile(path.join(__dirname, '../public/pages/404.html'));
 });
+
+
+
 
 
 
