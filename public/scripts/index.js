13a4e34b89c3698b69629a20e0675e460b9e59ff
Eric K <eric.koehli@mail.utoronto.ca>
2019-04-03 17:21:20 -0400

    fixed alignment on index.html

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -69,6 +69,10 @@ function init() {
         signInDiv.removeChild(signInDiv.lastElementChild);
         if (!user.isAdmin) {
             const a = document.createElement("a");
+            const profileSpan = document.createElement('span');
+            profileSpan.innerText = "My Profile";
+            profileSpan.className = "topBarText";
+            const brk = document.createElement('br');
             a.setAttribute("href", "/pages/userProfile.html");
             const imageContainer = document.createElement("div");
             imageContainer.className = "topBarImageContainer";
@@ -77,6 +81,8 @@ function init() {
             image.setAttribute("src", user.avatar);
             imageContainer.appendChild(image);
             a.appendChild(imageContainer);
+            a.appendChild(brk);
+            a.appendChild(profileSpan);
             imageContainer.appendChild(image);
             signInDiv.appendChild(a);
         } else {
