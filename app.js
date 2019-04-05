6949ac740d03b62d919c3ef12e5da20e22e2bbfe
will <will.qie@mail.utoronto.ca>
2019-04-04 22:10:38 -0400

    fix image, css, js reference for deployment

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Static directories
 app.use("/", express.static(__dirname + '/public'));
+app.use("/lightbox", express.static(__dirname + '/public/lightbox'));
 app.use("/pages", express.static(__dirname + '/public/pages'));
 app.use("/styles", express.static(__dirname + '/public/styles'));
 app.use("/scripts", express.static(__dirname + '/public/scripts'));
