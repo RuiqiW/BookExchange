a73e41fcea4876bf44ecf96eea26dab47d4d6502
will <will.qie@mail.utoronto.ca>
2019-04-04 21:06:52 -0400

    remove hardcode database link to support environmanetal variable

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Connect to our database
 // NOTE: This is a cloud deployed database, the username and password are both "server"
-const mongoDB = 'mongodb+srv://server:server@cluster0-pyppi.mongodb.net/test?retryWrites=true';
+const mongoDB = process.env.DATABASE_TOKEN;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true }).catch((error) => {
     console.log("Error Connecting Database Server!");
